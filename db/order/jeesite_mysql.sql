ea155135949e118d951078229f966f84c75ec78d
lindor <lindor888@163.com>
2015-12-02 18:19:26 +0800

    新增目录和产品

--- db/order/jeesite_mysql.sql
+++ db/order/jeesite_mysql.sql
@@ -31,7 +31,41 @@ CREATE TABLE sys_order
 	PRIMARY KEY (id)
 ) COMMENT = '订单';
 
+CREATE TABLE sys_col
+(
+	id varchar(64) NOT NULL COMMENT '编号',
+	parent_id varchar(64) NOT NULL COMMENT '父级编号',
+	parent_ids varchar(2000) NOT NULL COMMENT '所有父级编号',
+	name varchar(100) NOT NULL COMMENT '名称',
+	sort decimal(10,0) NOT NULL COMMENT '排序',
+	code varchar(100) COMMENT '类目编码',
+	lev char(1) COMMENT '类目层级',
+	create_by varchar(64) NOT NULL COMMENT '创建者',
+	create_date datetime NOT NULL COMMENT '创建时间',
+	update_by varchar(64) NOT NULL COMMENT '更新者',
+	update_date datetime NOT NULL COMMENT '更新时间',
+	remarks varchar(255) COMMENT '备注信息',
+	del_flag char(1) DEFAULT '0' NOT NULL COMMENT '删除标记',
+	PRIMARY KEY (id)
+) COMMENT = '类目表';
 
+
+CREATE TABLE sys_product
+(
+	id varchar(64) NOT NULL COMMENT '编号',
+	name varchar(100) NOT NULL COMMENT '名称',
+	code varchar(100) COMMENT '编码',
+	col_code varchar(100) NOT NULL COMMENT '类目编码',
+	sort decimal(10,0) NOT NULL COMMENT '排序',
+	price decimal(10,2) NOT NULL COMMENT '价格',
+	create_by varchar(64) NOT NULL COMMENT '创建者',
+	create_date datetime NOT NULL COMMENT '创建时间',
+	update_by varchar(64) NOT NULL COMMENT '更新者',
+	update_date datetime NOT NULL COMMENT '更新时间',
+	remarks varchar(255) COMMENT '备注信息',
+	del_flag char(1) DEFAULT '0' NOT NULL COMMENT '删除标记',
+	PRIMARY KEY (id)
+) COMMENT = '产品表';
 
 /* Create Indexes */
 
